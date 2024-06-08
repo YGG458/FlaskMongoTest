8ddb0c121c85fa70e9ccbfe17c6392ab5edf516f
Kwan <1229200091@qq.com>
2024-06-08 14:20:28 +1000

    v2

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
+import os
 
 def get_client():
-    connect_string = 'mongodb+srv://guan:Aa123456@abandon.wga0hxr.mongodb.net/?retryWrites=true&w=majority&appName=Abandon'
+    connect_string = os.getenv('MONGODB_URI')
+    if not connect_string:
+        raise ValueError("Environment variable MONGODB_URI is not set.")
     return MongoClient(connect_string)
 
 def get_database(client):
@@ -9,6 +12,4 @@ def get_database(client):
 
 def get_collection(database_name,collction_name):
     return database_name[collction_name]
-
-
 
